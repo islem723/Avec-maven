18a6063d2cdbc22c133bf68cd23e44cab4732038
Islem Naffeti <islem.naffeti@esprit.tn>
2022-03-13 23:09:49 +0100

    modif 2

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,6 +21,7 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 //new commit
+	//2éme modif
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
